c030c88c6ccbb4d0a8b9dfdf5615eedbfeeb38be
yooseez <yoosee@qq.com>
2026-08-25 21:08:09 +0800

    fix: add-template getAutoDate 时区处理（created_time UTC 转北京时间后再取日期）

--- add-template/parse_and_create.js
+++ add-template/parse_and_create.js
@@ -67,7 +67,9 @@ if (QUERY_BODY_FILE) {
 function getAutoDate(r) {
   const fillDate = r.properties['填报日期']?.date?.start;
   if (fillDate) return fillDate.includes('T') ? fillDate.split('T')[0].replace(/\//g, '-') : fillDate.replace(/\//g, '-');
-  return r.created_time.split('T')[0];
+  // created_time 为 UTC，需转为北京时间(+8)后再取日期，否则凌晨创建的记录会算到前一天
+  const beijing = new Date(new Date(r.created_time).getTime() + 8 * 3600 * 1000);
+  return beijing.toISOString().split('T')[0];
 }
 
 if (!SKIP_CHECK) {
